8187723f3f47d23d0409a38fb6c8d7eca6030f5d
Deeply04 <bakhutashvili19@gmail.com>
2022-09-10 14:39:34 +0400

    homework11

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+const group = {
+    name: 'davit',
+    lastName: 'bakhutashvili',
+    city: 'tbilisi',
+    age: 22,
+    isMerried: false,
+    isGraduated: true,
+    proggramingLanguages: ['html', 'css',],
+    drivingLicenses: ['A1', 'B', 'B1'],
+}
+
+console.log (group)
